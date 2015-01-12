b2a2de2db50349260472f320c64ff79fb3c802c6
yaniv51 <yaniv.israel@gmail.com>
2015-01-12 15:38:45 +0200

    added unit tests for both algorithms tested events: one step befor lose -> must block one step before win -> must make win move

---
+++ ServerSide/src/model/algorithm/MiniMaxTest.java
@@ -0,0 +1,34 @@
+package model.algorithm;
+
+import static org.junit.Assert.*;
+import model.domain.TicTacToeState;
+
+import org.junit.Test;
+
+public class MiniMaxTest {
+
+	@Test
+	public void testFirstLayer() {
+		//generate new board -> next computer move must block player at 2,0
+		int board[][] = new int[][] {{1,0,0,},{1,2,0},{0,0,0,} };
+		TicTacToeState state = new TicTacToeState(2, board, 10, 3, 0);
+		MiniMax ai = new MiniMax();
+		//set minimum depth for algorithm to see next player move
+		int depth = 2;
+		int move = ai.firstLayer(state, depth);
+		assertEquals(20,move);
+	}
+	
+	@Test
+	public void testFirstLayer1() {
+		// generate new board -> next computer move must win at this turn at 2,1
+		int board[][] = new int[][] { { 1, 0, 1, }, { 0, 1, 0 }, { 2, 0, 2, } };
+		TicTacToeState state = new TicTacToeState(2, board, 10, 3, 0);
+		MiniMax ai = new MiniMax();
+		// set minimum depth for algorithm to see next player move
+		int depth = 2;
+		int move = ai.firstLayer(state, depth);
+		assertEquals(21, move);
+	}
+
+}
