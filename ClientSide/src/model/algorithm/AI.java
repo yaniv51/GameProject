9181b6d349c36b459503fb89c8053fc1fa8bc6c9
Yehonatan <Yehonatan@Yehonatan-PC>
2015-01-22 08:28:50 +0200

    java doc up date

--- ClientSide/src/model/algorithm/AI.java
+++ ClientSide/src/model/algorithm/AI.java
@@ -2,11 +2,13 @@ package model.algorithm;
 
 public interface AI {
 	/**
+	 * <h1> firstLayer <h1> <p>
 	 * 
+	 * the method will Calculate the first layer of the algorithm and returns the best move that has found
 	 * 
 	 * @param state
 	 * @param maxDepth
-	 * @return
+	 * @return integer with the best score 
 	 */
 	public int firstLayer(State state, int maxDepth);
 	
